62bdff30a1d9f9bcdeb20fac5409c26d7216865a
raza <mehmood.kirmani@gmail.com>
2025-03-20 01:53:41 +0500

    docstring

---
+++ docstrng.py
@@ -0,0 +1,10 @@
+#docstring is basicaaly used to define what a function or class is doing just written after the fn defination
+
+def square(n):
+
+    '''takes a number and return square'''
+
+    print(n**2)
+
+square(5)
+print(square.__doc__)
