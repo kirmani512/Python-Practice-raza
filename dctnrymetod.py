f99ab0b423efce18518b604aada94f62267ed1bd
raza <mehmood.kirmani@gmail.com>
2025-03-25 00:43:48 +0500

    dictionary methods

---
+++ dctnrymetod.py
@@ -0,0 +1,17 @@
+user1={122:455,123:764,124:900}
+
+user2={125:653,126:983}
+
+# user1.update(user2)
+
+# user1.clear() #it clears all the items from user1
+
+# user1.pop(123) #removes only 122 key value pair
+
+# user1.popitem() #removes lasst key value pair
+
+# del user1 #deltes the dictionanry and you can see an error in output
+
+del user1[122] #only deletes this item
+
+print(user1)
