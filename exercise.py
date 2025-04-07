3f0dd830a5ba684a5ca4fcbe227400fe5b9ce227
raza <mehmood.kirmani@gmail.com>
2025-04-07 21:40:08 +0500

    kbc exercise

--- exercise.py
+++ exercise.py
@@ -1,10 +1,47 @@
-l=["pakistan birthday","1947"]
+#Self practice-------------------------------------------------------
 
-print(l[0])
+# l=["pakistan birthday","1947"]
 
-user_input=input()
+# print(l[0])
 
-if user_input ==l[1]:
-    print("get 100")
-else:
-    print("you lose")
+# user_input=input()
+
+# if user_input ==l[1]:
+#     print("get 100")
+# else:
+#     print("you lose")
+
+#final Solution------------------------------------------------------
+
+questions=[
+    ["which language is used in fb","php","js","python",1],
+    ["which language is used in fb","php","js","python",1],
+    ["which language is used in fb","php","js","python",1],
+    ["which language is used in fb","php","js","python",1],
+     ["which language is used in fb","php","js","python",1],
+    ["which language is used in fb","php","js","python",1],
+    ["which language is used in fb","php","js","python",1],
+    ["which language is used in fb","php","js","python",1],
+     ["which language is used in fb","php","js","python",1],
+    ["which language is used in fb","php","js","python",1],
+    ["which language is used in fb","php","js","python",1],
+    ["which language is used in fb","php","js","python",1],
+
+       ]
+
+level=[1000,2000,3000]
+amount=0
+for i in range(0,len(questions)):
+    question= questions[i]
+    print(f"question for rs {level[i]}")
+    print(f"a. {question[i][1]}                  b. {question[i][2]}                  c. {question[i][3]}")
+    reply=int(input("enter your answer between 1 to 3"  ))
+    if reply==question[-1]:
+        print(f"you win rs {level[i]}")
+        if(i==3):
+            money=10000
+        elif(i==9):
+          money=5000
+    else:
+        print("you lose")
+        break
