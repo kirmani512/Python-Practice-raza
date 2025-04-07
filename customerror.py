8a92f0975bb6805bac73e055c666f6cdeabc9a06
raza <mehmood.kirmani@gmail.com>
2025-04-07 13:16:00 +0500

    custom error

--- customerror.py
+++ customerror.py
@@ -4,7 +4,7 @@ if user_input.lower()=="quit":
     print("hi")
 
 else:
-    a-int(user_input)    
+    a=int(user_input)    
 
-if(a<4 or a>10):
-    raise ValueError("value should be between 4 to 10")
+    if(a<4 or a>10):
+        raise ValueError("value should be between 4 to 10")
