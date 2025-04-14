d41aa0df9905152e6d2fd85a8a39c6161ca8e233
raza <mehmood.kirmani@gmail.com>
2025-04-14 22:41:27 +0500

    if __name__==main

---
+++ ifnameis.py
@@ -0,0 +1,5 @@
+#Python's if __name__ == "__main__" idiom is used when code should be executed only when a file is run as a script rather than imported as a module. 
+
+import raza
+
+raza.welcome()
--- raza.py
+++ raza.py
@@ -2,3 +2,7 @@ def welcome():
     print("hi how are you")
 
 raza="A python developer"
+
+print(__name__)
+if __name__=="__ifnameis__":
+    welcome()
