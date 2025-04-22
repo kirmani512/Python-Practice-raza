30c65effbfc73f282282f43adfa97506d85c69fb
raza <mehmood.kirmani@gmail.com>
2025-04-22 14:26:34 +0500

    readline and writeline methods ni IO

--- filehandle.py
+++ filehandle.py
@@ -37,13 +37,48 @@
 
 #to append data
 
-f=open('myfile2.txt','a')
+# f=open('myfile2.txt','a')
 
-f.write('how are you')
+# f.write('how are you')
 
+# f.close()
+
+# #another method by using with---------------------------//////////////////////////////////
+# with open('myfile2.txt','a') as f:
+
+#     f.write("use of with")
+
+
+#readline method is used to read file line by line////////////////////////////////////////
+
+# f=open('myfile.txt','r')
+# while True:
+#     line=f.readline()
+#     if not line:
+#         break
+#     print (line)
+
+#another use case////////////////////////////////////////////////////////////////////////////////
+
+# f=open('myfile3.txt','r')
+# i=0
+# while True:
+#     i=i+1
+#     line=f.readline()
+#     if not line:
+#         break
+#     m1=int(line.split(",")[0])
+#     m3=int(line.split(",")[2])
+#     m2=int(line.split(",")[1])
+#     print(f"marks of student {i} in maths is: {m1*2}")
+#     print(f"marks of student {i} in English is: {m2*2}")
+#     print(f"marks of student {i} in Computer is: {m3*2}")
+
+    
+#     print(line)
+
+    #wrtieline method is used to write items of list to the file////////////////////////
+f=open('myfile3.txt','w')
+lines=['line 1\n','line 2\n','line 3\n']
+f.writelines(lines)
 f.close()
-
-#another method by using with---------------------------//////////////////////////////////
-with open('myfile2.txt','a') as f:
-
-    f.write("use of with")
