a43375343d53b3e30b9f02e200ac6d5302a54bd6
raza <mehmood.kirmani@gmail.com>
2025-04-30 16:37:17 +0500

    difference between is and ==

---
+++ iskeyword.py
@@ -0,0 +1,10 @@
+# differencce between is and == ///////////////////////////////////////
+
+a=[1,2,3,4,7]
+
+b=[1,2,3,4,7]
+
+#both works same but the main deifference is
+print(a is b)  #compares exact location of object in memory
+
+print(a==b)    #compares value
