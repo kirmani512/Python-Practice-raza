95f2df2e2f5c95612a1c05d7cf772a1185a7d27a
raza <mehmood.kirmani@gmail.com>
2025-07-04 21:21:44 +0500

    command line utility

---
+++ command_line.py
@@ -0,0 +1,21 @@
+# A command line utility in python is usually a programm designd to be executed directly 
+# through command line.
+
+
+import argparse
+
+def main():
+    parser=argparse.ArgumentParser(description="calculator")  #argparse.ArgumentParser this is the class inside argparse module
+
+    parser.add_argument("x", type=float, help="first number")
+    parser.add_argument("y",type=float,help="second number")
+    parser.add_argument("--add",action="store_true",help="add numbers")
+
+    args=parser.parse_args() #here parer is the command line parser --------------- .parse_args() reaads what the user typed in the terminal
+
+    if args.add:
+        result=args.x+args.y
+        print(f"result is {result}")
+
+if __name__ == "__main__":
+    main()
