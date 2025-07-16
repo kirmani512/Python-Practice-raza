0c57b056e3b25420c4fc2dbf9e4061dbeb7871b5
raza <mehmood.kirmani@gmail.com>
2025-07-16 17:10:34 +0500

    shutil module

---
+++ shutill.py
@@ -0,0 +1,14 @@
+# it offers a number of high level operations on files and collection of files
+
+import shutil
+
+# shutil.copy("shutill.py","shutill1.py") #it makes the new file by copying shutill.py
+
+# shutil.copytree(".tutorial","my tutorial") #it copies all the folder content to new folder
+
+# shutil.move(".tutorial/file.py","file1.py") #moves file outside the folder
+
+shutil.rmtree("my tutorial") # deletes my tutorial folder
+
+
+
